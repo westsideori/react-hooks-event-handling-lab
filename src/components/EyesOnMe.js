3f98b65bed6b941b0f6ea22a1d7006673d2824d1
Ori Markowitz <ori.markowitz@gmail.com>
2021-02-01 18:25:18 -0500

    commit tree 98951af27d536cacd75a299f51918e493280c4fe

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function blurText() {
+        console.log("Hey! Eyes on me!")
+    }
+
+    function focusText() {
+        console.log("Good!")
+    }
+
+
+    return (
+        <button onBlur={blurText} onFocus={focusText}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+function Keypad() {
+
+    function enteringPass() {
+        console.log("Entering password...")
+    }
+
+    return (
+        <input onChange={enteringPass} type="password"></input>
+    )
+}
+
+export default Keypad;
